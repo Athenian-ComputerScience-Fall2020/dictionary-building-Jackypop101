208fdd81e0f57f04a20896b93b586e17e455d074
Jackypop101 <23hchen@athenian.org>
2020-10-21 19:24:33 +0000

    Finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,16 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  https://www.w3schools.com/python/gloss_python_dictionary_add_item.asp
 
+dictionary = {}
+while True:
+    a = input('What is your name?')
+    b = input('What is your dream job?')
+    dictionary[str(a)] = str(b)
+    c = input ("Please pass it on to the next person and enter c to continue and s to stop.")
+    if c == 'c':
+        print('lets get started')
+    elif c == 's':
+        break
+
+for key, value in dictionary.items():
+    print(key + " wants to be a " + value)
